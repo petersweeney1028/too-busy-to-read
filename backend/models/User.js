144c58662bc6e825e040e5b80a0d364ec001e811
Peter <petersweeney1028@gmail.com>
2024-06-28 15:58:18 -0400

    set up google oauth and other backend stuff

--- backend/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  googleId: String,
-  email: String,
-  name: String
-});
-
-module.exports = mongoose.model('User', UserSchema);
